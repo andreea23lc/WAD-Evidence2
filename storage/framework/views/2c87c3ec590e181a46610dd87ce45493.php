eeb9f4d6e06b4c933f2c696ed7c2ea5b7c03fb07
andreea23lc <109566430+andreea23lc@users.noreply.github.com>
2024-04-07 16:14:18 -0600

    Add files via upload

---
+++ storage/framework/views/2c87c3ec590e181a46610dd87ce45493.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to the Library Management System</h1>
+    </header>
+    <main>
+        <?php echo $__env->yieldContent('content'); ?>
+    </main>
+</body>
+</html>
+<?php /**PATH C:\Users\Al028\library-MS\resources\views/welcome.blade.php ENDPATH**/ ?>
