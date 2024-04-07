ba7514418ea103a6366d168f05b5a47502b43fea
andreea23lc <109566430+andreea23lc@users.noreply.github.com>
2024-04-07 16:14:01 -0600

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/books', 'BookController@index')->name('books.index');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{id}/update', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{id}/destroy', [BookController::class, 'destroy'])->name('books.destroy');
